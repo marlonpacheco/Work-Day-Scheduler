b8cf43e928810209f621879d82966f01b3a36efa
Marlon Pacheco <pacheco.mjc@gmail.com>
2020-08-01 23:42:47 -0500

    added all rows and updated styling

--- assets/script.js
+++ assets/script.js
@@ -2,28 +2,16 @@
 var todayEl = moment().format('dddd, MMMM Do YYYY')
 var containerEl = document.querySelector(".container")
 
+var time9 = document.getElementById("9am")
+var time10 = document.getElementById("10am")
+var time11 = document.getElementById("11am")
+var time12 = document.getElementById("12pm")
+var time1 = document.getElementById("1pm")
+var time2 = document.getElementById("2pm")
+var time3 = document.getElementById("3pm")
+var time4 = document.getElementById("4pm")
+var time5 = document.getElementById("5pm")
+
+
 // displays current day and date in header 
 document.getElementById("currentDay").innerText = todayEl
-
-var row9am = moment('10 00').format('hh:mm')
-
-var row = [{
-    hour : "time",
-    event : "event",
-    save : "save",
-}]
-
-function addRows (){
-    for (var i=0; i < 9; i++){
-
-        var row1 = [{
-            hour : "time",
-            event : arrary[0],
-            save : "save",
-        }]
-    }
-}
-
-addRows()
-
-var array = ["t","10am","11am"]
