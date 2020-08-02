d5b7425da544bd2edd0106cdd5598582220a333d
Marlon Pacheco <pacheco.mjc@gmail.com>
2020-08-01 22:59:33 -0500

    determined it would be best to one row per hour instead of a for loop

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,29 @@
 
-var todayEl = new Date();
-var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var todayEl = moment().format('dddd, MMMM Do YYYY')
+var containerEl = document.querySelector(".container")
 
+// displays current day and date in header 
+document.getElementById("currentDay").innerText = todayEl
 
-document.getElementById("currentDay").innerText = "Today is: " + days[todayEl.getDay()] +", " + months[todayEl.getMonth()] + " " + todayEl.getDate() + ", " + todayEl.getFullYear();
+var row9am = moment('10 00').format('hh:mm')
 
+var row = [{
+    hour : "time",
+    event : "event",
+    save : "save",
+}]
 
+function addRows (){
+    for (var i=0; i < 9; i++){
 
+        var row1 = [{
+            hour : "time",
+            event : arrary[0],
+            save : "save",
+        }]
+    }
+}
 
+addRows()
 
+var array = ["t","10am","11am"]
