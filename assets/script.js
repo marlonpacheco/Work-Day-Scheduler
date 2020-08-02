1c462a5954d236229f66f4cc0e79ba74ad3cdaef
Marlon Pacheco <pacheco.mjc@gmail.com>
2020-08-02 18:54:46 -0500

    fixed some things. still could not figure out retrieving data from local storage

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,9 @@ var todayEl = moment().format('dddd, MMMM Do YYYY')
 var containerEl = document.querySelector(".container")
 
 var time9 = document.getElementById("9am")
+var button9 = document.querySelector("#btn9")
+var time9 = document.getElementById("event9")
+
 var time10 = document.getElementById("10am")
 var time11 = document.getElementById("11am")
 var time12 = document.getElementById("12pm")
@@ -15,3 +18,15 @@ var time5 = document.getElementById("5pm")
 
 // displays current day and date in header 
 document.getElementById("currentDay").innerText = todayEl
+
+var timeRN = moment().format("H")
+
+if( timeRN > 19){
+    document.getElementById("event9").classList.add("past");
+}if( timeRN < 19){
+    document.getElementById("event9").classList.add("future");
+}if(timeRN == 19 ) {
+    document.getElementById("event9").classList.add("present");
+}
+
+console.log(timeRN)
