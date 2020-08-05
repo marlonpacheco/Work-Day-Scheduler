b171a93ed9106a5899352e737643579ca22428e2
Marlon Pacheco <pacheco.mjc@gmail.com>
2020-08-04 22:23:01 -0500

    tried but failed to do for loop for changing past, present, future class so did it the long way

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Work-Day-Scheduler
+
+This is day work scheduler using JSON
+
+
+
+## Screen Shot
+![](assets/ScreenShot.png)
+
+
+## Link
+https://marlonpacheco.github.io/Work-Day-Scheduler/
--- assets/script.js
+++ assets/script.js
@@ -11,25 +11,96 @@ var time3 = document.getElementById("event3")
 var time4 = document.getElementById("event4")
 var time5 = document.getElementById("event5")
 
-//retrieves "time attribute value"
-var timeValue = document.querySelectorAll(".description")[0].getAttribute("time")
-
+//gets the hour value from current time//
+var timeRN = moment().format("H")
+console.log(timeRN)
 
 // displays current day and date in header 
 document.getElementById("currentDay").innerText = todayEl
 
-//gets the hour value from current time//
-var timeRN = moment().format("H")
-console.log(timeRN)
-//sets class to format depending on current time
-if( timeRN> timeValue){
+
+// sets class to format depending on current time
+if( timeRN> 9){
     time9.classList.add("past");
 }
-if( timeRN < 19){
+if( timeRN < 9){
     time9.classList.add("future");
 }
-if(timeRN == 19 ) {
+if(timeRN == 9 ) {
     time9.classList.add("present");
+}
+
+if( timeRN> 10){
+    time10.classList.add("past");
+}
+if( timeRN < 10){
+    time10.classList.add("future");
+}
+if(timeRN == 10 ) {
+    time10.classList.add("present");
+}
+if( timeRN> 11){
+    time11.classList.add("past");
+}
+if( timeRN < 11){
+    time11.classList.add("future");
+}
+if(timeRN == 11 ) {
+    time11.classList.add("present");
+}
+if( timeRN> 12){
+    time12.classList.add("past");
+}
+if( timeRN < 12){
+    time12.classList.add("future");
+}
+if(timeRN == 12 ) {
+    time12.classList.add("present");
+}
+if( timeRN> 1){
+    time1.classList.add("past");
+}
+if( timeRN < 1){
+    time1.classList.add("future");
+}
+if(timeRN == 1 ) {
+    time1.classList.add("present");
+}
+if( timeRN> 2){
+    time2.classList.add("past");
+}
+if( timeRN < 2){
+    time2.classList.add("future");
+}
+if(timeRN == 2 ) {
+    time2.classList.add("present");
+}
+if( timeRN> 3){
+    time3.classList.add("past");
+}
+if( timeRN < 3){
+    time3.classList.add("future");
+}
+if(timeRN == 3 ) {
+    time3.classList.add("present");
+}
+if( timeRN> 4){
+    time4.classList.add("past");
+}
+if( timeRN < 4){
+    time4.classList.add("future");
+}
+if(timeRN == 4 ) {
+    time4.classList.add("present");
+}
+if( timeRN> 5){
+    time5.classList.add("past");
+}
+if( timeRN < 5){
+    time5.classList.add("future");
+}
+if(timeRN == 5 ) {
+    time5.classList.add("present");
 }
 
 //retrives values from local storage when refreshed//
@@ -75,3 +146,21 @@ $("#btn4").on("click", function () {
 $("#btn5").on("click", function () {
     localStorage.setItem("5pm",time5.value)
 })
+
+// was trying to do for loop instead for changing "past, present future" class.....still work in progress
+//retrieves "time attribute value"
+// var timeValue = document.querySelectorAll(".description")[0].getAttribute("time")
+
+// for (var i = 0;i<8;i++){
+//     var rowTime = $(".description")[i].getAttribute("time")
+//     var selectedRow = $("")
+//     if (timeRN > rowTime){
+//         $("input").addClass("past")
+//     }
+//     if (timeRN <rowTime){
+//         $("input").addClass("future")
+//     }
+//     if (timeRN == rowTime){
+//         $("input").addClass("present")
+//     }
+// }
